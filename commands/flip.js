55eefd13cb9961208d2af61d24610f05381d736d
David Jerome <GlassToeStudio@gmail.com>
2017-07-17 11:35:06 -0500

    update

--- commands/flip.js
+++ commands/flip.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js')
 const Confax = require('../bot.js')
 const request = require('request')
 
-Confax.registerCommand('dog', 'default', (message) => {
-  let results = (Math.round((Math.random())) == 1) ? 'heads' : 'tails'
+Confax.registerCommand('flip', 'default', (message) => {
+    let results = (Math.round((Math.random())) == 1) ? 'heads' : 'tails'
     message.channel.send(results)
 
 }, ['flip'], 'flip a coin', '[]')
