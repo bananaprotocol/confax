7067fb96180320c316d4487e8b5943f8d12802dc
David Jerome <GlassToeStudio@gmail.com>
2017-07-16 22:01:37 -0500

    Just unflip it

--- modules/tableunflip.js
+++ modules/tableunflip.js
@@ -6,29 +6,29 @@ const GlassBot = require('../bot.js')
 const bot = GlassBot.bot
     //const config = GlassBot.config
 
-var isFlipped = false
-
-// bot.on('message', message => {
-//     if (message.author.bot) return
-//     if (message.content.includes("(╯°□°）╯︵ ┻━┻"))
-//         message.channel.send("┬─┬ ノ( ゜-゜ノ)")
-//     return
-// });
+//var isFlipped = false
 
 bot.on('message', message => {
     if (message.author.bot) return
-
-    if (message.content.includes("(╯°□°）╯︵ ┻━┻")) {
-        if (!isFlipped) {
-            isFlipped = true
-            return
-        } else {
-            message.delete()
-            message.channel.send("Table is already flipped.. you can't flip a table thats flipped.")
-            return
-        }
-    } else if (message.content.includes("┬─┬﻿ ノ( ゜-゜ノ)"))
-        isFlipped = false
-
+    if (message.content.includes("(╯°□°）╯︵ ┻━┻"))
+        message.channel.send("┬─┬ ノ( ゜-゜ノ)")
     return
 });
+
+// bot.on('message', message => {
+//     if (message.author.bot) return
+
+//     if (message.content.includes("(╯°□°）╯︵ ┻━┻")) {
+//         if (!isFlipped) {
+//             isFlipped = true
+//             return
+//         } else {
+//             message.delete()
+//             message.channel.send("Table is already flipped.. you can't flip a table thats flipped.")
+//             return
+//         }
+//     } else if (message.content.includes("┬─┬﻿ ノ( ゜-゜ノ)"))
+//         isFlipped = false
+
+//     return
+// });
