46aac00077269ce76268e943b3b08e6f31d44cb9
David Jerome <GlassToeStudio@gmail.com>
2017-07-17 16:51:06 -0500

    remove these

--- commands/flip.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const Confax = require('../bot.js')
-const request = require('request')
-
-Confax.registerCommand('flip', 'default', (message) => {
-    let results = (Math.round((Math.random())) == 1) ? 'heads' : 'tails'
-    message.channel.send(results)
-
-}, ['flip'], 'flip a coin', '[]')
