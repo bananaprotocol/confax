defaea1c3b16a29934bc79df416d580138769afb
Glass Toe Studio <GlassToeStudio@gmail.com>
2017-07-17 13:50:16 -0500

    Add files via upload

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,18 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/)
 
+## [0.4.1]
+### Changed
+- Fixed typo in the Code Formatter ([checkforcode.js](./modules/checkforcode.js))
+
+## [0.4.0]
+### Added
+- Code Formatter (C# only for now)
+
+## [0.3.1]
+### Security
+- Update Node.js version to resolve security issues
+
 ## [0.3.0]
 ### Added
 - Dog command
@@ -24,7 +36,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/)
 
 ## [0.1.0]
 ### Added
-- Advanced Help, Command Help, Eval, Invite, Math, Mute, Purge and Uptime Commands 
+- Advanced Help, Command Help, Eval, Invite, Math, Mute, Purge and Uptime Commands
 
 ## [0.0.1]
 ### Added
--- bot.js
+++ bot.js
@@ -34,7 +34,7 @@ var loadScript = (path, reload) => {
 }
 
 function changeConfig (guildID, callback) {
-  var path = './GlassBotfiles/' + guildID + '.yml'
+  var path = './confaxfiles/' + guildID + '.yml'
   var data = yaml.safeLoad(fs.readFileSync(path))
   callback()
   fs.writeFileSync(path, yaml.safeDump(data))
@@ -42,7 +42,7 @@ function changeConfig (guildID, callback) {
 }
 
 function getConfigValue (guildID, name) {
-  var path = './GlassBotfiles/' + guildID + '.yml'
+  var path = './confaxfiles/' + guildID + '.yml'
   var data = yaml.safeLoad(fs.readFileSync(path))
   try { return data[name] } catch (error) { console.log('An error occured: ' + error.stack) }
 }
@@ -52,12 +52,12 @@ exports.loadScript = loadScript
 exports.changeConfig = changeConfig
 exports.getConfigValue = getConfigValue
 
-//var commands = fs.readdirSync('./commands/')
-//commands.forEach(script => {
-// if (script.substring(script.length - 3, script.length) === '.js') {
-//    exports.loadScript('./commands/' + script)
-//  }
-//})
+var commands = fs.readdirSync('./commands/')
+commands.forEach(script => {
+  if (script.substring(script.length - 3, script.length) === '.js') {
+    exports.loadScript('./commands/' + script)
+  }
+})
 
 var modules = fs.readdirSync('./modules/')
 modules.forEach(script => {
