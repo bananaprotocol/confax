0ed2ab1181b9178c6b809a447e6ad849e6a81a93
TheOnlyArtz <gamesil456@gmail.com>
2017-07-31 23:21:41 +0300

    Getting real ping

--- commands/ping.js
+++ commands/ping.js
@@ -3,5 +3,6 @@ const Confax = require('../bot.js')
 
 Confax.registerCommand('ping', 'default', (message, bot) => {
   var time = Date.now()
-  message.channel.send('Pong!').then(m => m.edit('Pong! `took ' + (Date.now() - time) + 'ms`'))
+  message.channel.send('Pong!')
+    .then(m => m.edit(`Pong! took ${m.createdTimestamp - message.createdTimestamp}`)
 }, ['response'], 'Bot Response Test', '[]')
