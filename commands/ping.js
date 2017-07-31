b3029b7367da2d71d8e8814f958718635c387824
TheOnlyArtz <gamesil456@gmail.com>
2017-07-31 23:23:26 +0300

    Fixed indents and tokens

--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,8 @@ const Confax = require('../bot.js')
 
 Confax.registerCommand('ping', 'default', (message, bot) => {
   var time = Date.now()
-  message.channel.send('Pong!')
-    .then(m => m.edit(`Pong! took ${m.createdTimestamp - message.createdTimestamp}`)
+  message.channel.send('Pinging...')
+    .then(m => {
+      m.edit(`Pong! took \`${m.createdTimestamp - message.createdTimestamp}\`ms`)
+    })
 }, ['response'], 'Bot Response Test', '[]')
