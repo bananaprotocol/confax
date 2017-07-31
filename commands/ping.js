9429dd172713d1fa7106a2b7aface353085ffddf
TheOnlyArtz <gamesil456@gmail.com>
2017-07-31 23:27:56 +0300

    No need for that

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js')
 const Confax = require('../bot.js')
 
 Confax.registerCommand('ping', 'default', (message, bot) => {
-  var time = Date.now()
   message.channel.send('Pinging...')
     .then(m => {
       m.edit(`Pong! took \`${m.createdTimestamp - message.createdTimestamp}\`ms`)
