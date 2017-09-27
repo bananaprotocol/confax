d8f1c4f7ddb83e68f651daeb3ed7c5ef8a3070b3
Glass Toe Studio <GlassToeStudio@gmail.com>
2017-09-27 07:03:18 -0500

    Delete initial messge
    If bot has permission, delete the command message and respond with ( ͡° ͜ʖ ͡°)

--- commands/lenny.js
+++ commands/lenny.js
@@ -1,6 +1,12 @@
-const Discord = require('discord.js')
 const Confax = require('../bot.js')
 
-Confax.registerCommand('lenny', 'default', (message, bot) => {  
-  message.channel.send('( ͡° ͜ʖ ͡°)')
-}, ['lenny'], 'its lenny ffs', '["!lenny"]')
+Confax.registerCommand('lenny', 'default', (message, bot) => {
+  let managePerms = message.guild.member(bot.user).hasPermission('MANAGE_MESSAGES')
+  if (managePerms) {
+    message.delete()
+    message.channel.send('( ͡° ͜ʖ ͡°)')
+  } else {
+    message.channel.send('( ͡° ͜ʖ ͡°)')
+  }
+}, ['lenny'], 'its lenny ffs', '<!lenny>')
+
