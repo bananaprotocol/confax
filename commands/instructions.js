c47132c7dd5f6dccd3c9a7a07a58e953466ca07c
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-01-27 12:44:03 +0200

    Glass asked me to replace his code with this

--- commands/instructions.js
+++ commands/instructions.js
@@ -1,11 +1,12 @@
 const Confax = require('../bot.js')
 
 Confax.registerCommand('instructions', 'default', (message, bot) => {
-    return("\nSIMPLE INSTRUCTIONS:\n"  +
-    "``` \n" +
-    "1. Go to Hastenbin/Pastebin\n" +
-    "2. Paste your FULL script\n" +
-    "3. Click save and generate link\n" +
-    "4. Paste link here.\n" +
-    "```" )
-}, ['paste', 'haste', 'pastebin', 'hastebin'], 'Simple Instuctions to post a link to your code', '<!haste>')
+  return ('\nSIMPLE INSTRUCTIONS:\n' +
+          '``` \n' +
+          '1. Go to Codeshare.io\n' +
+          '2. Click "Share Code Now"' +
+          '3. Paste your FULL script\n' +
+          '3. Click the little gear on the right and choose your code language\n' +
+          '4. Copy the URL and Paste link here.\n' +
+          '```')
+}, ['paste', 'codeshare'], 'Simple Instuctions to post a link to your code', '<!haste>')
