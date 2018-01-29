bf348d630c3d9863aba42abb694df27a87f65a41
David Jerome <GlassToeStudio@gmail.com>
2018-01-29 16:13:26 -0600

    Update crypto to Rich emeds, aded map of currency symbols. Fixed typo in emptyquestion.

--- commands/crypto.js
+++ commands/crypto.js
@@ -1,6 +1,6 @@
-/*  crypto.js by David Jerome @GlassToeStudio - GlassToeStudio@gmail.com
+/*  crypto.js @GlassToeStudio - GlassToeStudio@gmail.com
 
-    9 September, 2017
+    16 September, 2017
     https://github.com/GlassToeStudio
     http://glasstoestudio.weebly.com/
     https://twitter.com/GlassToeStudio
@@ -8,7 +8,7 @@
     ------------------------------------------------------------------------
     Return current price of a given crypto-currency in $US
 
-    The following command is uisng a free API from https://www.cryptocompare.com/ (CC BY-NC 3.0)
+    The following command is using a free API from https://www.cryptocompare.com/ (CC BY-NC 3.0)
     The API request can be found here: https://www.cryptocompare.com/api#-api-data-price-
 
     Usage:
@@ -25,7 +25,8 @@
 */
 const GlassBot = require('../bot.js')
 const request = require('request')
-const Util = require('util')
+const Discord = require('discord.js')
+const currencySymbolMap = require('../map')
 
 GlassBot.registerCommand('crypto', 'default', (message, bot) => {
   //  Init variables
@@ -44,66 +45,33 @@ GlassBot.registerCommand('crypto', 'default', (message, bot) => {
 function CrytpoComparePrice (fsym, tsyms, message) {
   //  The API call string.
   let cryptoComparePrice = 'https://min-api.cryptocompare.com/data/price?fsym=' + fsym + '&tsyms=' + tsyms
+
   request(cryptoComparePrice, function (error, response, body) {
     if (error) message.channel.send('Not a valid crypto currency, try BTC or ETH.')
-    let finalMessage = '**~~------~~** __Current exchange rates for 1 ' + fsym + '__ **~~------~~**\n```'
+    let embed = new Discord.RichEmbed().setTitle('cryptocompare.com')
+      .setDescription('```MarkDown\n#_Current exchange rates for 1 ' + fsym + '```')
+      .setURL('https://www.cryptocompare.com/api/')
+      .setColor(586901)
+      .setTimestamp()
     try {
       let responseBody = JSON.parse(body)
       for (let sym = 0; sym < tsyms.length; sym++) {
         let price = responseBody[tsyms[sym]]
         if (price === undefined) price = '¯\\_(ツ)_/¯' // Not valid crypto currency ¯\_(ツ)_/¯
-        if (tsyms[sym] === 'USD') price = '$' + price
-        finalMessage += Util.format('%s %s\n', pad('.............', tsyms[sym], false), price)
+        else if (getSymbolFromCurrency(tsyms[sym]) !== undefined) price = ' ' + getSymbolFromCurrency(tsyms[sym]) + ' ' + parseFloat(price).toLocaleString()
+        else price = parseFloat(price).toLocaleString()
+        embed.addField('*  ' + tsyms[sym], '```js\n' + price + '```', true)
       }
-      message.channel.send(finalMessage + '```')
+      message.channel.send('**' + fsym + '**', {embed})
     } catch (error) {
       message.channel.send('<:doggo:328259712963313665>' + ' Not a valid crypto-currency, try BTC or ETH.')
     }
   })
 }
 
-/**
- * Pad a string on the left or right for better alignment.
- * @param  {string} pad
- * @param  {string} str
- * @param  {boolean} padLeft
- */
-function pad (pad, str, padLeft) {
-  if (typeof str === 'undefined') return pad
-  if (padLeft) return (pad + str).slice(-pad.length)
-  else return (str + pad).substring(0, pad.length)
+function getSymbolFromCurrency (currencyCode) {
+  if (typeof currencyCode !== 'string') return undefined
+  var code = currencyCode.toUpperCase()
+  if (!currencySymbolMap.hasOwnProperty(code)) return undefined
+  return currencySymbolMap[code]
 }
-
-// /*  crypto.js by David Jerome @GlassToeStudio - GlassToeStudio@gmail.com
-
-//     9 September, 2017
-//     https://github.com/GlassToeStudio
-//     http://glasstoestudio.weebly.com/
-//     https://twitter.com/GlassToeStudio
-
-//     ------------------------------------------------------------------------
-//     Return current price of a given crypto-currency in $US
-//     The command uses the API from: http://www.coincap.io/
-//     The API can be found here: https://github.com/CoinCapDev/CoinCap.io
-// */
-// const GlassBot = require('../bot.js')
-// const request = require('request')
-
-// GlassBot.registerCommand('crypto', 'default', (message, bot) => {
-//   let address = 'http://www.coincap.io/page/' + message.content.toUpperCase()
-//   request(address, function (error, response, body) {
-//     if (error) {
-//       //  error is always null, but put this here for jstandard: (Expected error to be handled.)
-//       message.channel.send('Not a valied crypto currency, try BTC.')
-//       return
-//     }
-//     try {
-//       let btcBody = JSON.parse(body)
-//       let str = btcBody.price_usd.toString()
-//       let price = str.slice(0, str.indexOf('.') + 3)
-//       message.channel.send('The current price for 1 **' + message.content.toUpperCase() + '** is $**' + price + '** USD.').catch(err => console.log(err.stack))
-//     } catch (error) {
-//       message.channel.send('<:doggo:328259712963313665>' + ' Not a valid crypto-currency, try BTC or ETH.')
-//     }
-//   })
-// }, ['crypt', 'price'], 'Get latest crypto currency price.', '[]')
