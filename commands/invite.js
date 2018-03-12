e40bc6ce402ae6a4577bbf9c6aebcf665df5675d
bananaprotocol <thebananaprotocol@gmail.com>
2018-03-12 21:04:57 +0100

    Change invite link in the invite command
    Change the invite link to include permissions

--- commands/invite.js
+++ commands/invite.js
@@ -2,5 +2,5 @@ const Discord = require('discord.js')
 const Confax = require('../bot.js')
 
 Confax.registerCommand('invite', 'default', (message) => {
-  return 'Invite Link: https://discordapp.com/oauth2/authorize?client_id=319545839951544320&scope=bot'
+  return 'Invite Link: https://discordapp.com/oauth2/authorize?client_id=319545839951544320&permissions=519174&scope=bot'
 }, ['invitelink'], 'Get invite link to invite Confax to your server', '[]')
