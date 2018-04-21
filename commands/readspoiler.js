c4a5ed61f2c8068ae47dfcc817180f5f8cec14b7
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-04-22 01:57:23 +0300

    Update !readspoiler command
    Add catch statement for users who have disabled DMs

--- commands/readspoiler.js
+++ commands/readspoiler.js
@@ -2,7 +2,10 @@ const Confax = require('../bot.js')
 const spoilerMsg = require('./spoiler.js')
 
 Confax.registerCommand('readspoiler', 'default', (message, bot) => {
-  message.react("👍")
   let combinedMessage = '**Originally posted by** ' + spoilerMsg.spoilerMsgAuthor + '\n**Content:** ' + spoilerMsg.spoilerMsgContent
-  message.author.send(combinedMessage)
+  message.author.send(combinedMessage).then(() => {message.react("👍")})
+    .catch((error) => {
+      message.reply("Please enable DMs to receive the message!")
+      message.react("😞")
+    })
 }, ['tellmethesecret', 'sendmemsg'], 'Read latest spoiler (content will be sent via DMs)', '')
