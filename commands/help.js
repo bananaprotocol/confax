442f5dda5887ba612dd0e249b0dac82f43b92807
Glass Toe Studio <GlassToeStudio@gmail.com>
2018-04-25 20:49:12 -0500

    Update help command

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,4 @@
-const Discord = require('discord.js')
 const Confax = require('../bot.js')
-const config = require('../config.json')
 
 Confax.registerCommand('help', 'default', (message, bot) => {
   let commands = Confax.commands
@@ -14,11 +12,6 @@ Confax.registerCommand('help', 'default', (message, bot) => {
   for (let loopCmdType in commands) {
     for (let loopCmd in commands[loopCmdType]) {
       cmds[loopCmdType].push(loopCmd)
-/*      let aliases = commands[loopCmdType][loopCmd].aliases
-        for (let i = 0; i < aliases.length; i++) {
-        let alias = aliases[i]
-        cmds[loopCmdType].push(alias)
-      } */ // Remove aliases from default command output
     }
   }
 
@@ -29,10 +22,12 @@ Confax.registerCommand('help', 'default', (message, bot) => {
   let defaultcmds = Object.keys(cmds['default']).length
   let dmcmds = Object.keys(cmds['dm']).length
 
-  return 'Default Commands **(' + defaultcmds + ')** ```' + cmds['default'].join(' \n') + ' ```\n' +
-         'DM Commands **(' + dmcmds + ')** ```' + cmds['dm'].join(' \n') + ' ```\n' +
-         'Moderator Commands **(' + modcmds + ')** ```' + cmds['moderator'].join(' \n') + ' ```\n' +
-         'Master Commands **(' + mastercmds + ')** ```' + cmds['master'].join(' \n') + ' ```\n' +
-         'All Commands - **(' + (defaultcmds + dmcmds + modcmds + mastercmds) + ')**' +
-         '```Use advancedhelp to get an advanced list of all commands or cmdhelp to get a detailed description of one. ```'
+  let helpList = '\n'
+  if (defaultcmds > 0) helpList += 'Default Commands **(' + defaultcmds + ')** ```' + cmds['default'].join(' \n') + ' ```\n'
+  if (dmcmds > 0) helpList += 'DM Commands **(' + dmcmds + ')** ```' + cmds['dm'].join('-\n') + ' ```\n'
+  if (modcmds > 0) helpList += 'Moderator Commands **(' + modcmds + ')** ```' + cmds['moderator'].join(' \n') + ' ```\n'
+  if (mastercmds > 0) helpList += 'Master Commands **(' + mastercmds + ')** ```' + cmds['master'].join(' \n') + ' ```\n'
+  helpList += 'All Commands - **(' + (defaultcmds + dmcmds + modcmds + mastercmds) + ')**' +
+  '```Use advancedhelp to get an advanced list of all commands or cmdhelp to get a detailed description of one. ```'
+  return helpList
 }, ['cmds', 'commands', 'commandlist'], 'List all commands', '[]')
