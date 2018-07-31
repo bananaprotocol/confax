ebe6748cca30fe777ca68f506b232d37bbdf7031
bananaprotocol <bananaprotcol@protonmail.com>
2018-07-30 23:40:34 -0400

    Fix dank meme command

--- commands/dankmeme.js
+++ commands/dankmeme.js
@@ -1,51 +1,50 @@
 const Confax = require('../bot.js')
 const Discord = require('discord.js')
 const https = require('https')
-const url = 'https://www.reddit.com/r/dankmemes/hot/.json?limit=100'
+const url = 'https://www.reddit.com/r/dankmemes.json?limit=100'
 
-Confax.registerCommand('meme', 'default', (message, bot) => {
+Confax.registerCommand('dankmeme', 'default', (message, bot) => {
   https.get(url, (result) => {
-    var body = ''
+    let body = ''
     result.on('data', (chunk) => {
       body += chunk
     })
-    
 
     result.on('end', () => {
-      var response = JSON.parse(body)
-      var index = response.data.children[Math.floor(Math.random() * 99) + 1].data
+      let response = JSON.parse(body)
+      let index = response.data.children[Math.floor(Math.random() * 99) + 1].data
+      let subRedditName = index.subreddit_name_prefixed
+      let title = index.title
+      let link = 'https://www.reddit.com' + index.permalink
+      let text = index.selftext
       if (index.post_hint !== 'image') {
-        var text = index.selftext
-        const textembed = new Discord.RichEmbed()
+        const textEmbed = new Discord.RichEmbed()
           .setTitle(subRedditName)
           .setColor(9384170)
           .setDescription(`[${title}](${link})\n\n${text}`)
-          .setURL(`https://reddit.com/${subRedditName}`)
+          .setURL(`https://www.reddit.com/${subRedditName}`)
 
-        message.channel.send(textembed)
+        message.channel.send(textEmbed)
       }
-      var image = index.preview.images[0].source.url
-      var title = index.title
-      var link = 'https://reddit.com' + index.permalink
-      var subRedditName = index.subreddit_name_prefixed
+      let image = index.preview.images[0].source.url
       if (index.post_hint !== 'image') {
-        const textembed = new Discord.RichEmbed()
+        const textEmbed = new Discord.RichEmbed()
           .setTitle(subRedditName)
           .setColor(9384170)
           .setDescription(`[${title}](${link})\n\n${text}`)
-          .setURL(`https://reddit.com/${subRedditName}`)
+          .setURL(`https://www.reddit.com/${subRedditName}`)
 
-        message.channel.send(textembed)
+        message.channel.send(textEmbed)
       }
-      const imageembed = new Discord.RichEmbed()
+      const imageEmbed = new Discord.RichEmbed()
         .setTitle(subRedditName)
         .setImage(image)
         .setColor(9384170)
         .setDescription(`[${title}](${link})`)
-        .setURL(`https://reddit.com/${subRedditName}`)
-      message.channel.send(imageembed)
+        .setURL(`https://www.reddit.com/${subRedditName}`)
+      message.channel.send(imageEmbed)
     }).on('error', function (e) {
       console.log('Got an error: ', e)
     })
   })
-}, ['dank', 'dankmeme'], 'Generate Random memes', '')
+}, ['dank', 'meme'], 'Get a random meme from r/dankmemes (potentially NSFW)', '[]')
