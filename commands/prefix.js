c53bd1325c6d345928d5da32e56152de7f7efbcf
Chroventer <chroventer@gmail.com>
2019-03-16 22:01:51 +0400

    feat(prefix): add prefix command

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js')
 const Confax = require('../bot.js')
 
-Confax.registerCommand('prefix', 'default', (message) => {
-  return '**Prefix has been changed!**'
+Confax.registerCommand('prefix', 'moderator', (message, bot) => {
+  const args = message.content.split(' ');
+  const prefix = args.join(' ');
+  if (!prefix) return message.reply('Sorry, but you must specify what you want to set the custom server prefix as.');
+  bot.prefixes.set(message.guild.id, prefix);
+  const embed = new Discord.RichEmbed()
+    .setTitle('Prefix Set!')
+    .setDescription(`The server custom prefix has been set to ${prefix}`)
+    .setColor('RANDOM');
+  return message.channel.send(embed);
 }, ['setprefix', 'changeprefix'], 'Changes the custom guild prefix.', '[]')
