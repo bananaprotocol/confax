2e291dad5f6239234b06c21d0b29a0ce88965f60
= <sietse85@gmail.com>
2020-08-14 21:16:44 +0200

    Export right way, fix crash when no guild-id in banwords.js

--- src/modules/banwords.js
+++ src/modules/banwords.js
@@ -4,13 +4,15 @@ const bot = Confax.bot
 const warnedUserIds = Confax.warnedUserIds
 
 bot.on('message', (message) => {
+  if (message.guild.id === null) {
+    return message
+  }
   const config = Confax.getConfig(message.guild.id)
   const bannedWords = config.bannedWords
 
   if (message.author.bot) return
-
   const lowercaseMessage = message.content.toLowerCase()
-  for (var word in bannedWords) {
+  for (const word in bannedWords) {
     if (lowercaseMessage.split(' ').indexOf(bannedWords[word]) > -1) {
       const apiCommands = ['addbannedword', 'abw', 'removebannedword', 'rbw']
       if (apiCommands.some(x => { return lowercaseMessage.indexOf(x) >= 0 })) {
